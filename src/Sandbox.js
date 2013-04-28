9167ec81d5bf8adfd621021c1d914b83aebbeea1
gaoxiaochen <gxcsoccer@126.com>
2013-04-28 14:48:58 +0800

    make some change

--- src/Sandbox.js
+++ src/Sandbox.js
@@ -4,42 +4,82 @@
 		};
 
 	var preDefinedConfig = {
-		totalParticles: 400,
-		emissionRate: 400 / 2,
-		pos: {
-			x: 400,
-			y: 300
+		waterfall: {
+			totalParticles: 400,
+			emissionRate: 400 / 2,
+			pos: {
+				x: 400,
+				y: 300
+			},
+			posVar: {
+				x: 0,
+				y: 20
+			},
+			gravity: {
+				x: 0,
+				y: 100
+			},
+			life: 10,
+			lifeVar: 5,
+			radius: 5,
+			radiusVar: 3,
+			angle: 90,
+			angleVar: 180,
+			speed: 80,
+			speedVar: 20,
+			startScale: 1,
+			endScale: 1,
+			tangentialAccel: 0,
+			tangentialAccelVar: 0,
+			radialAccel: 0,
+			radialAccelVar: 0,
+			startColor: [19.89, 59.93, 255, 1],
+			startColorVar: [0, 0, 48, 0.3],
+			endColor: [198.9, 198.9, 255, 0],
+			endColorVar: [0, 0, 0, 0],
+			// initPos: function(pos) {
+			// 	var r = Util.toRad(pos.x);
+			// 	return new Vector2d(Math.cos(r) * 60, Math.sin(r) * 80);
+			// }
 		},
-		posVar: {
-			x: 0,
-			y: 20
-		},
-		gravity: {
-			x: 0,
-			y: 100
-		},
-		life: 10,
-		lifeVar: 5,
-		radius: 5,
-		radiusVar: 3,
-		angle: 90,
-		angleVar: 180,
-		speed: 80,
-		speedVar: 20,
-		startScale: 1,
-		endScale: 1,
-		tangentialAccel: 0,
-		tangentialAccelVar: 0,
-		radialAccel: 0,
-		radialAccelVar: 0,
-		startColor: [19.89, 59.93, 255, 1],
-		startColorVar: [0, 0, 48, 0.3],
-		endColor: [198.9, 198.9, 255, 0],
-		endColorVar: [0, 0, 0, 0],
-		// initPos: function(pos) {
-		// 	var r = Util.toRad(pos.x);
-		// 	return new Vector2d(Math.cos(r) * 60, Math.sin(r) * 80);
-		// }
+		ring: {
+			totalParticles: 400,
+			emissionRate: 400 / 2,
+			pos: {
+				x: 400,
+				y: 300
+			},
+			posVar: {
+				x: 180,
+				y: 20
+			},
+			gravity: {
+				x: 0,
+				y: 0
+			},
+			life: 2.5,
+			lifeVar: 1,
+			radius: 5,
+			radiusVar: 3,
+			angle: 90,
+			angleVar: 180,
+			speed: 0,
+			speedVar: 0,
+			startScale: 1,
+			endScale: 0.6,
+			tangentialAccel: 480,
+			tangentialAccelVar: 30,
+			radialAccel: -480,
+			radialAccelVar: 30,
+			startColor: [19.89, 59.93, 255, 1],
+			startColorVar: [0, 0, 48, 0.3],
+			endColor: [198.9, 198.9, 255, 0],
+			endColorVar: [0, 0, 0, 0],
+			initPos: function(pos) {
+				var r = Util.toRad(pos.x);
+				return new Vector2d(Math.cos(r) * 60, Math.sin(r) * 80);
+			}
+		}
 	};
 
 	var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame ||
@@ -67,7 +107,7 @@
 		init: function() {
 			canvas = $('canvas');
 			context = canvas.getContext('2d');
-			emitter = new ParticleEmitter(preDefinedConfig);
+			emitter = new ParticleEmitter(preDefinedConfig.ring);
 
 			play();
 		}
